ae6f7162c35f0dcca438f0b8b99b8fe9047b6c10
Data Engineer | Databases & Cloud Data Services in training <lucianamelisa@gmail.com>
2022-03-10 17:40:57 -0300

    carga en repositorio de la práctica unidad 2

---
+++ Unidad-2/ejercicioPracticon2-1.js
@@ -0,0 +1,39 @@
+/*Declaración y asignación de valores a la variable. 
+Reasignación de valores de Variables en Javascript. 
+Trabajo con código Js de manera externa al archivo HTML.*/ 
+
+/*Declarar una variable de nombre cantidadDeVentanasEnMiCasa y asignarle la cantidad de ventanas 
+que hay en tu casa. Mostrar el valor de la variable por la consola.*/
+ 
+var cantidadDeVentanasEnMiCasa; 
+cantidadDeVentanasEnMiCasa = 5; 
+console.log(cantidadDeVentanasEnMiCasa);
+
+/*Declarar una variable de nombre soyHumano y asignarle el valor true (verdadero). Mostrar el valor 
+de la variable por la consola.*/
+
+var soyHumano;
+soyHumano = true; 
+console.log(soyHumano); 
+
+/*Declarar una variable de nombre miGustoDePizzaPreferido y asignarle la variedad de pizza que más te 
+guste usando un String (cadena de caracteres). Mostrar el valor de la variable por la consola.*/
+
+var miGustoDePizzaPreferido; 
+miGustoDePizzaPreferido = "Vegetales Primavera"; 
+console.log(miGustoDePizzaPreferido); 
+
+/*Mostrar por la consola los siguientes valores false, 'Hola', 0 y 'Minions' usando la misma variable.*/
+
+var mutable; 
+mutable = false; 
+console.log(mutable); 
+
+mutable = "Hola"; 
+console.log(mutable); 
+
+mutable = 0; 
+console.log(mutable); 
+
+mutable = "Minions"; 
+console.log(mutable);
