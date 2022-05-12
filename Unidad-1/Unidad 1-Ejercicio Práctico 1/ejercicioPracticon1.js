f2c7cde913b533cf6d8a76b9277d168279a0fcab
Data Engineer | Python - SQL  - Cloud Computing <lucianamelisa@gmail.com>
2022-05-12 05:11:58 -0300

    ADD: unit 1 & 2 exercises
    JavaScript exercises that I have done on the mini-bootcamp
    Issue: #N°11

---
+++ Unidad-1/Unidad 1-Ejercicio Práctico 1/ejercicioPracticon1.js
@@ -0,0 +1,8 @@
+/*Práctica con el objeto global console, que permite mostrar información en la consola junto con el 
+método .log, trabajando con código Js con archivo externo a HTML. Más un alert con un mensaje inicial.*/
+
+alert("Hola! Ve a Inspeccionar -> Console -> y verifica mis datos cargados"); 
+console.log("a) El día de mi cumpleaños es: " + 4);
+console.log("b) El mes de mi cumpleaños es: " + "Julio"); 
+console.log("c) El año de mi nacimiento es: " + 1985);
+console.log("d) Mi número favorito es: " + 7); 
