2552e1ba8962c68faf221ad673c606f4bbffa896
Data Engineer | Python - SQL  - Cloud Computing <lucianamelisa@gmail.com>
2022-05-12 04:50:46 -0300

    DELETE: to reload de repo from git remote
    To reload the repository from remote and with tasks divided by activity
    Issue: #N°9

--- Unidad 1/Unidad 1-Ejercicio Práctico 1/ejercicioPracticon1.js
+++
@@ -1,8 +0,0 @@
-/*Práctica con el objeto global console, que permite mostrar información en la terminal junto con el 
-método .log, trabajando con código Js con archivo externo a HTML. Más un alert con un mensaje inicial.*/
-
-alert("Hola! Ve a Inspeccionar -> Console -> y verifica mis datos cargados"); 
-console.log("a) El día de mi cumpleaños es:", 4);
-console.log("b) El mes de mi cumpleaños es:", 7); 
-console.log("c) El año de mi nacimiento es:", 1985);
-console.log("d) Mi número favorito es:", 7); 
