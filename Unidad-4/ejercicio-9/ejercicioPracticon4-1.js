06e1a1ea0fd45c0210e57fb5483ca37940776e42
Data Engineer | Python - SQL  - Cloud Computing <lucianamelisa@gmail.com>
2022-05-13 00:32:35 -0300

    ADD: exercise 9 - unit 4
    Issue: #N°17

--- Unidad-4/ejercicio-9/ejercicioPracticon4-1.js
+++ Unidad-4/ejercicio-9/ejercicioPracticon4-1.js
@@ -5,3 +5,19 @@ trabajo con CONSTANTES, prompt (input de usuario), alert (output por pantalla). 
 /*9. Una farmacia hace el 15% de descuento a sus clientes si la compra es mayor a 100 pesos. Escribir un 
 programa que dado el monto de una compra como valor numérico, diga cuanto debe abonar el cliente aplicando 
 el descuento si es necesario.*/
+
+var compra = Number(prompt("Ingrese el monto de la compra: ")); 
+const DESCUENTO = 15; 
+var montoTotalCompra;
+
+if (compra > 100) {
+    //bloque de código para compras superiores a $100,00 
+    
+    montoTotalCompra = compra*(1-(DESCUENTO/100));
+} else {
+    //bloque de código para compras iguales o menores a $100,00
+
+    montoTotalCompra = compra; 
+}
+
+document.write("El monto total a pagar por la compra es $" + montoTotalCompra.toFixed(2));
